0239841b27aa15e57820ded4ed57534a0f51585b
KennyLual <klual1998@gmail.com>
2023-05-25 16:23:19 -0400

    Added capstone files

---
+++ script.js
@@ -0,0 +1,21 @@
+let autocomplete;
+function initAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById("autocomplete"),
+        {
+            fields: ["place_id", "geometry", "name"],
+        }
+    );
+}
+autocomplete.addListener("place_changed", onPlaceChanged);
+function onPlaceChanged() {
+    var place = autocomplete.getPlace();
+
+    if (!place.geometry) {
+        // Use did not select a prediction; reset the input field
+        document.getElementById("autocomplete").placeholder = "Enter a place";
+    } else {
+        // Display details about the valid place
+        document.getElementById("details").innerHTML = place.name;
+    }
+}
